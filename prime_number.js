1b1402bdb03e27967f34457a8b8db4b54fb828ff
Victor <ankitsingh40068@gmail.com>
2022-09-20 19:10:37 +0530

    commit 1

---
+++ prime_number.js
@@ -0,0 +1,13 @@
+let num = 13;
+
+console.log(primeCheck(num));
+
+function primeCheck(){ 
+if(num <=1 || num == undefined){ 
+return false;
+}
+
+for(let i = 2; i <= num**0.5; i++)
+{ if(num % i == 0){  return false; } }
+return true;
+}
